0c7020fb6e9961892b73d0d929e3009ea7f956e3
Caio Sousa <caiofmsousa@gmail.com>
2024-11-18 09:35:41 -0300

    Improved navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,30 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import ROUTES from "../../routes/routes";
 import "./index.css";
+import { BsArrowReturnLeft } from "react-icons/bs";
+
+const ROUTES_LOOKUP = Object.fromEntries(
+  Object.values(ROUTES).map((route) => [route.path, route])
+);
 
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const matchedRoute = ROUTES_LOOKUP[location.pathname];
+
   return (
-    <header onClick={() => navigate(ROUTES.home.path)}>Score Validator</header>
+    <header>
+      {location.pathname !== "/" && (
+        <BsArrowReturnLeft
+          className="back-button"
+          role="button"
+          size={32}
+          onClick={() => navigate(ROUTES.home.path)}
+        />
+      )}
+      {matchedRoute?.name || "Unknown Route"}
+    </header>
   );
 };
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,20 +2,25 @@ import Home from "./home/index.tsx";
 import IndividualForm from "./individualForm/index.tsx";
 import CompanyForm from "./companyForm/index.tsx";
 
-const ROUTES: Record<string, { path: string; component: React.ComponentType }> =
-  {
-    home: {
-      path: "/",
-      component: Home,
-    },
-    individualForm: {
-      path: "/formulario-individual",
-      component: IndividualForm,
-    },
-    companyForm: {
-      path: "/formulario-empresarial",
-      component: CompanyForm,
-    },
-  };
+const ROUTES: Record<
+  string,
+  { name: string; path: string; component: React.ComponentType }
+> = {
+  home: {
+    name: "Inicio",
+    path: "/",
+    component: Home,
+  },
+  individualForm: {
+    name: "Formulário Individual",
+    path: "/formulario-individual",
+    component: IndividualForm,
+  },
+  companyForm: {
+    name: "Formulário Empresarial",
+    path: "/formulario-empresarial",
+    component: CompanyForm,
+  },
+};
 
 export default ROUTES;
