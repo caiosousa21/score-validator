daba8c4f12c74f6a92e5fad2279a05b10229d006
Caio Sousa <caiofmsousa@gmail.com>
2024-11-19 14:11:09 -0300

    wrap await logic

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-const { worker } = await import("./mocks/browser");
 
-await worker.start();
+async function startApp() {
+  const { worker } = await import("./mocks/browser");
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+  await worker.start();
+
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+startApp().catch((error) => {
+  console.error("Failed to start the app:", error);
+});
